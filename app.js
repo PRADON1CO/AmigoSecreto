5da219dca95833ccb57ebaa968753addf90f49c3
Prado Nicolas <nicolas88prado@gmail.com>
2025-02-01 22:12:19 -0300

    agregue la funcion agregarAmigo y tambien agregue la classe btnEliminar con sus estilos necesarios

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo(){
+    const amigoInput = document.getElementById('amigo');
+    const amigo = amigoInput.value.trim();
+
+    if (amigo !== ''){
+        const lista = document.getElementById('listaAmigos');
+
+        let listaItem = document.createElement('li');
+        listaItem.textContent = amigo; 
+
+        lista.appendChild(listaItem); 
+        const buttonEliminar = document.createElement('button');
+        buttonEliminar.className='btnEliminar';
+        buttonEliminar.innerHTML = 'Eliminar';
+
+        buttonEliminar.onclick = function(){
+            lista.removeChild(listaItem)
+        }
+        listaItem.appendChild(buttonEliminar);
+        lista.appendChild(listaItem);
+
+        amigoInput.value = '';
+
+    }else{
+        alert('Agregue a un amigo por favor');
+    }
+}
